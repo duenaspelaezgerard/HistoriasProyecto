0cfd9e947acf25321c519d0a5bb01c3e051c71b0
Gerard Duenas <duenaspelaezgerard@fpllefia.com>
2024-05-14 18:07:14 +0200

    librerias

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import {NextUIProvider} from '@nextui-org/react'
-import './index.css'
+// main.tsx or main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {NextUIProvider} from "@nextui-org/react";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <NextUIProvider>
+    <NextUIProvider>
+      <main className="purple-dark text-foreground bg-background">
         <App />
+      </main>
     </NextUIProvider>
   </React.StrictMode>,
-)
+);
