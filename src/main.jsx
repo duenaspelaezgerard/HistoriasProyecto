16d8f30daad4ff5ae8c2500e7dacf19c975519d2
Gerard Duenas <duenaspelaezgerard@fpllefia.com>
2024-05-17 15:57:44 +0200

    historia3 acabada

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import { NextUIProvider } from "@nextui-org/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { GlobalProvider } from "./context/GlobalContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <main className="purple-dark text-foreground">
-        <App />
-      </main>
-    </NextUIProvider>
+    <GlobalProvider>
+      <NextUIProvider>
+        <main className="purple-dark text-foreground">
+          <App />
+        </main>
+      </NextUIProvider>
+    </GlobalProvider>
   </React.StrictMode>,
 );
